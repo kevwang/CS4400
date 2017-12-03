9f1eca0988423779a23d49990c89d1d82c5744ee
kevwang <kev.wang2@gmail.com>
2017-12-03 18:57:24 -0500

    updAteZ

--- src/sls/RegistrationController.java
+++ src/sls/RegistrationController.java
@@ -1,13 +1,19 @@
 package sls;
 
+import db.CardQueries;
 import db.DBUserQueries;
+import db.UserCardManagementQueries;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
+import models.Breezecard;
+import models.SuspendedCard;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class RegistrationController {
@@ -57,6 +63,39 @@ public class RegistrationController {
 
         if (DBUserQueries.register(user.getText(), email.getText(), pass.getText(),
                 cardNumber.getText().replaceAll("\\s+",""))) {
+
+            // Check if the created card is now suspended
+            List<SuspendedCard> susCards = CardQueries.getSuspendedCards();
+            List<Breezecard> brCards = UserCardManagementQueries.getCards();
+            /*for (SuspendedCard susCard : susCards) {
+                for (Breezecard brCard : brCards) {
+                    if (susCard.getBreezecardNumber().equals(brCard.getCardNumber())) {
+                        Random rand = new Random();
+                        cardNumber.setText((1000 + rand.nextInt(9000)) + " " +
+                                (1000 + rand.nextInt(9000)) + " " +
+                                (1000 + rand.nextInt(9000)) + " " +
+                                (1000 + rand.nextInt(9000)) + " ");
+                        UserCardManagementQueries.addCard(cardNumber.getText());
+
+                        Alert alert = new Alert(Alert.AlertType.INFORMATION,
+                                "That card is suspended. New card created toO");
+                        alert.showAndWait();
+                    }
+                }
+            }*/
+            if (brCards.isEmpty()) {
+                Random rand = new Random();
+                cardNumber.setText((1000 + rand.nextInt(9000)) + " " +
+                        (1000 + rand.nextInt(9000)) + " " +
+                        (1000 + rand.nextInt(9000)) + " " +
+                        (1000 + rand.nextInt(9000)) + " ");
+                UserCardManagementQueries.addCard(
+                        cardNumber.getText().replaceAll("\\s+",""));
+                Alert alert = new Alert(Alert.AlertType.INFORMATION,
+                        "That card is suspended. New card created toO");
+                alert.showAndWait();
+            }
+
             ViewManager.changeView(ScreensEnum.PASSENGER_WELCOME);
         } else {
             Alert alert = new Alert(Alert.AlertType.ERROR, "There was a problem making your account");
